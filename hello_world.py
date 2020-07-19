eca2453b06c4ccccb3896ad18048ad7cc715135b
iepi <chenxh0722@126.com>
2020-07-19 08:06:09 +0800

    Signed-off-by: iepi <chenxh0722@126.com>

---
+++ hello_world.py
@@ -0,0 +1,62 @@
+'''
+message = 'Hello Python world!'
+print(message)
+
+message = "Hello Python Crash Courese world!"
+
+name = "ada lovelace"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+first_name = "ada"
+last_name = "lovelace"
+full_name = f"{first_name} {last_name}"
+print(full_name)
+print(f"hello,{full_name.title()}!")
+
+message = f"hello,{full_name.title()}!"
+print(message)
+
+print("Python")
+print("\tPython")
+print("\nPython")
+
+print("Languages:\n\tPython\n\tC\n\tJavaScript")
+
+
+favorite_language = 'Python '
+print(favorite_language)
+print(len(favorite_language))
+a = favorite_language.rstrip()
+print(a)
+print(len(a))
+f = ' python '
+f1 = f.rstrip()
+f2 = f.lstrip()
+f3 = f.strip()
+print(f"{f1} {f2} {f3} {len(f1)} {len(f2)} {len(f3)}")
+'''
+'''
+message = "One of Python's trengths is its diverse commnity."
+print(message)
+
+name = 'iepi'
+message = f'hello,{name},what is the name of your daugther?'
+print(message)
+print(name.title(),name.upper(),name.lower())
+'''
+
+mingrenmingyan = '\tAlbert Einstein once said,"A person who never a \n\tmistake never tried anything new."'
+print(mingrenmingyan)
+
+universe_age = 12_000_000_000
+print(universe_age)
+
+x,y,z = 0,0,0
+MAX_CONNECTIONS = 5000
+number = 37
+message = f"what my favoriter number is {number}"
+print(message)
+
+import this
